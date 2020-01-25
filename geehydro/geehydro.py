2ec1338436e192eb9eaad2e0fea91692fd88c566
Qiusheng Wu <giswqs@gmail.com>
2020-01-24 22:05:58 -0500

    Updated addLayer function

--- geehydro/geehydro.py
+++ geehydro/geehydro.py
@@ -6,15 +6,56 @@ import os
 import ee
 import folium
 from folium import plugins
-# from add_layer import add_ee_layer
-# from basemaps import basemaps
 
-# ee.Initialize()
+
+def addLayer(self, ee_object, vis_params={}, name='Layer untitled', shown=True, opacity=1):
+
+    image = None
+
+    if not isinstance(ee_object, ee.Image) and not isinstance(ee_object, ee.ImageCollection) and not isinstance(ee_object, ee.FeatureCollection) and not isinstance(ee_object, ee.Feature) and not isinstance(ee_object, ee.Geometry):
+        err_str = "\n\nThe image argument in 'addLayer' function must be an instace of one of ee.Image, ee.Geometry, ee.Feature or ee.FeatureCollection."
+        raise AttributeError(err_str)
+
+    if isinstance(ee_object, ee.geometry.Geometry) or isinstance(ee_object, ee.feature.Feature) or isinstance(ee_object, ee.featurecollection.FeatureCollection):
+        features = ee.FeatureCollection(ee_object)
+
+        width = 2
+
+        if 'width' in vis_params:
+            width = vis_params['width']
+
+        color = '000000'
+
+        if 'color' in vis_params:
+            color = vis_params['color']
+
+        image_fill = features.style(
+            **{'fillColor': color}).updateMask(ee.Image.constant(0.5))
+        image_outline = features.style(
+            **{'color': color, 'fillColor': '00000000', 'width': width})
+
+        image = image_fill.blend(image_outline)
+    elif isinstance(ee_object, ee.image.Image):
+        image = ee_object
+    elif isinstance(ee_object, ee.imagecollection.ImageCollection):    
+        image = ee_object.median()
+    
+
+    map_id_dict = ee.Image(image).getMapId(vis_params)
+    folium.raster_layers.TileLayer(
+        tiles = map_id_dict['tile_fetcher'].url_format,
+        attr = 'Google Earth Engine',
+        name = name,
+        overlay = True,
+        control = True,
+        show=shown,
+        opacity=opacity
+    ).add_to(self)
 
 
 
 # Adds a given EE object to the map as a layer.
-def addLayer(self, ee_object, vis_params={}, name='Layer', shown=True, opacity=1):
+def addLayer2(self, ee_object, vis_params={}, name='Layer', shown=True, opacity=1):
     
     try:    
         # display ee.Image()
@@ -71,6 +112,8 @@ def addLayer(self, ee_object, vis_params={}, name='Layer', shown=True, opacity=1
     
 # Add EE drawing method to folium.
 folium.Map.addLayer = addLayer
+folium.Map.addLayer2 = addLayer2
+
 
 # Modifies the Google Maps basemap
 # A mapTypeId to set the basemap to. Can be one of "ROADMAP", "SATELLITE", "HYBRID" or "TERRAIN" to select one of the standard Google Maps API map types
@@ -141,34 +184,36 @@ folium.Map.setCenter = setCenter
 
 
 def centerObject(self, ee_object, zoom):
-    try:    
+    # try:    
 
-        lat = 0
-        lon = 0
+    lat = 0
+    lon = 0
+    bounds = [[lat, lon], [lat, lon]]
+    if isinstance(ee_object, ee.geometry.Geometry):    
+        centroid = ee_object.centroid()
+        lon, lat = centroid.getInfo()['coordinates']
         bounds = [[lat, lon], [lat, lon]]
-        if isinstance(ee_object, ee.geometry.Geometry):    
-            centroid = ee_object.centroid()
-            lon, lat = centroid.getInfo()['coordinates']
-            bounds = [[lat, lon], [lat, lon]]
-        elif isinstance(ee_object, ee.featurecollection.FeatureCollection):    
-            centroid = ee_object.geometry().centroid()
-            lon, lat = centroid.getInfo()['coordinates']
-            bounds = [[lat, lon], [lat, lon]]
-        elif isinstance(ee_object, ee.image.Image):  
-            geometry = ee_object.geometry()  
-            coordinates = geometry.getInfo()['coordinates'][0]
-            bounds = [coordinates[0], coordinates[2]]
-        elif isinstance(ee_object, ee.imagecollection.ImageCollection):  
-            geometry = ee_object.geometry()  
-            coordinates = geometry.getInfo()['coordinates'][0]
-            bounds = [coordinates[0], coordinates[2]] 
-        else:
-            bounds = [[0, 0], [0, 0]]      
+    elif isinstance(ee_object, ee.featurecollection.FeatureCollection):    
+        centroid = ee_object.geometry().centroid()
+        lon, lat = centroid.getInfo()['coordinates']
+        bounds = [[lat, lon], [lat, lon]]
+    elif isinstance(ee_object, ee.image.Image):  
+        geometry = ee_object.geometry()  
+        print(geometry.getInfo())
+        coordinates = geometry.getInfo()['coordinates'][0]
+        bounds = [coordinates[0][::-1], coordinates[2][::-1]]
+    elif isinstance(ee_object, ee.imagecollection.ImageCollection):  
+        geometry = ee_object.geometry()  
+        coordinates = geometry.getInfo()['coordinates'][0]
+        bounds = [coordinates[0][::-1], coordinates[2][::-1]] 
+    else:
+        bounds = [[0, 0], [0, 0]]      
 
-        self.fit_bounds(bounds, max_zoom=zoom)
+    print(bounds)
+    self.fit_bounds(bounds, max_zoom=zoom)
 
-    except:
-        print("Failed to centerObject")
+    # except:
+    #     print("Failed to centerObject")
 
 folium.Map.centerObject = centerObject
 
