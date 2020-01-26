0e2c403ad66c337e029e48db14f3ceef3ddc3847
Qiusheng Wu <giswqs@gmail.com>
2020-01-26 11:42:01 -0500

    Added utilities.py

---
+++ examples/landsat_wrs2_grid.py
@@ -0,0 +1,37 @@
+'''
+<table class="ee-notebook-buttons" align="left"><td>
+<a target="_blank"  href="https://colab.research.google.com/github/giswqs/geehydro/blob/master/examples/geehydro-tutorials.ipynb">
+    <img src="https://www.tensorflow.org/images/colab_logo_32px.png" /> Run in Google Colab</a>
+</td><td>
+<a target="_blank"  href="https://github.com/giswqs/geehydro/tree/master/examples/geehydro-tutorials.ipynb"><img width=32px src="https://www.tensorflow.org/images/GitHub-Mark-32px.png" /> View source on GitHub</a></td><td>
+<a target="_blank"  href="https://nbviewer.jupyter.org/github/giswqs/geehydro/blob/master/examples/geehydro-tutorials.ipynb"><img width=26px src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/883px-Jupyter_logo.svg.png" />Notebook Viewer</a></td><td>
+<a target="_blank"  href="https://mybinder.org/v2/gh/giswqs/geehydro/master?filepath=Folium%2Fee-api-folium-setup.ipynb"><img width=58px src="https://mybinder.org/static/images/logo_social.png" />Run in binder</a></td></table>
+'''
+
+# %%
+!pip install geehydro
+
+# %%
+import ee
+import folium
+import geehydro
+
+# %%
+ee.Authenticate()
+ee.Initialize()
+
+# %%
+Map = folium.Map(location=[40, -100], zoom_start=4)
+Map.setOptions('HYBRID')
+
+# %%
+dataset = ee.FeatureCollection('projects/google/wrs2_descending')
+empty = ee.Image().byte()
+
+# %%
+Map.setCenter(-78, 36, 8)
+Map.addLayer(empty.paint(dataset, 0, 2), {}, 'Landsat WRS-2 grid')
+Map.setControlVisibility(layerControl=True, fullscreenControl=True, latLngPopup=True)
+Map
+
+# %%
--- geehydro/geehydro.py
+++ geehydro/geehydro.py
@@ -37,24 +37,23 @@ def addLayer(self, ee_object, vis_params={}, name='Layer untitled', shown=True, 
         image = image_fill.blend(image_outline)
     elif isinstance(ee_object, ee.image.Image):
         image = ee_object
-    elif isinstance(ee_object, ee.imagecollection.ImageCollection):    
+    elif isinstance(ee_object, ee.imagecollection.ImageCollection):
         image = ee_object.median()
-    
 
     map_id_dict = ee.Image(image).getMapId(vis_params)
     folium.raster_layers.TileLayer(
-        tiles = map_id_dict['tile_fetcher'].url_format,
-        attr = 'Google Earth Engine',
-        name = name,
-        overlay = True,
-        control = True,
+        tiles=map_id_dict['tile_fetcher'].url_format,
+        attr='Google Earth Engine',
+        name=name,
+        overlay=True,
+        control=True,
         show=shown,
         opacity=opacity
     ).add_to(self)
-    
+
+
 # Add EE drawing method to folium.
 folium.Map.addLayer = addLayer
-
 
 
 # Modifies the Google Maps basemap
@@ -63,39 +62,39 @@ def setOptions(self, mapTypeId='HYBRID', styles={}, types=[]):
     # Add custom basemaps to folium
     basemaps = {
         'ROADMAP': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Maps',
-            overlay = True,
-            control = True
+            tiles='https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+            attr='Google',
+            name='Google Maps',
+            overlay=True,
+            control=True
         ),
         'SATELLITE': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Satellite',
-            overlay = True,
-            control = True
+            tiles='https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+            attr='Google',
+            name='Google Satellite',
+            overlay=True,
+            control=True
         ),
         'TERRAIN': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Terrain',
-            overlay = True,
-            control = True
+            tiles='https://mt1.google.com/vt/lyrs=p&x={x}&y={y}&z={z}',
+            attr='Google',
+            name='Google Terrain',
+            overlay=True,
+            control=True
         ),
         'HYBRID': folium.TileLayer(
-            tiles = 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
-            attr = 'Google',
-            name = 'Google Satellite',
-            overlay = True,
-            control = True
+            tiles='https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}',
+            attr='Google',
+            name='Google Satellite',
+            overlay=True,
+            control=True
         ),
         'ESRI': folium.TileLayer(
-            tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-            attr = 'Esri',
-            name = 'Esri Satellite',
-            overlay = True,
-            control = True
+            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Satellite',
+            overlay=True,
+            control=True
         )
     }
 
@@ -104,9 +103,12 @@ def setOptions(self, mapTypeId='HYBRID', styles={}, types=[]):
     except:
         print('Basemap can only be one of "ROADMAP", "SATELLITE", "HYBRID", "TERRAIN", or "ESRI"')
 
+
 folium.Map.setOptions = setOptions
 
 # show the map
+
+
 def setControlVisibility(self, layerControl=True, fullscreenControl=True, latLngPopup=True):
 
     if layerControl:
@@ -116,61 +118,61 @@ def setControlVisibility(self, layerControl=True, fullscreenControl=True, latLng
     if latLngPopup:
         folium.LatLngPopup().add_to(self)
 
+
 folium.Map.setControlVisibility = setControlVisibility
 
 
 def setCenter(self, lon, lat, zoom=10):
     self.fit_bounds([[lat, lon], [lat, lon]], max_zoom=zoom)
-    
+
+
 folium.Map.setCenter = setCenter
 
 
 def centerObject(self, ee_object, zoom):
-    # try:    
+    # try:
 
     lat = 0
     lon = 0
     bounds = [[lat, lon], [lat, lon]]
-    if isinstance(ee_object, ee.geometry.Geometry):    
+    if isinstance(ee_object, ee.geometry.Geometry):
         centroid = ee_object.centroid()
         lon, lat = centroid.getInfo()['coordinates']
         bounds = [[lat, lon], [lat, lon]]
-    elif isinstance(ee_object, ee.featurecollection.FeatureCollection):    
+    elif isinstance(ee_object, ee.featurecollection.FeatureCollection):
         centroid = ee_object.geometry().centroid()
         lon, lat = centroid.getInfo()['coordinates']
         bounds = [[lat, lon], [lat, lon]]
-    elif isinstance(ee_object, ee.image.Image):  
-        geometry = ee_object.geometry()  
-        print(geometry.getInfo())
+    elif isinstance(ee_object, ee.image.Image):
+        geometry = ee_object.geometry()
         coordinates = geometry.getInfo()['coordinates'][0]
         bounds = [coordinates[0][::-1], coordinates[2][::-1]]
-    elif isinstance(ee_object, ee.imagecollection.ImageCollection):  
-        geometry = ee_object.geometry()  
+    elif isinstance(ee_object, ee.imagecollection.ImageCollection):
+        geometry = ee_object.geometry()
         coordinates = geometry.getInfo()['coordinates'][0]
-        bounds = [coordinates[0][::-1], coordinates[2][::-1]] 
+        bounds = [coordinates[0][::-1], coordinates[2][::-1]]
     else:
-        bounds = [[0, 0], [0, 0]]      
+        bounds = [[0, 0], [0, 0]]
 
-    print(bounds)
     self.fit_bounds(bounds, max_zoom=zoom)
 
     # except:
     #     print("Failed to centerObject")
 
-folium.Map.centerObject = centerObject
 
+folium.Map.centerObject = centerObject
 
 
 if __name__ == '__main__':
 
     ee.Initialize()
-    
+
     dem = ee.Image('USGS/SRTMGL1_003')
     # Set visualization parameters.
     vis_params = {
-    'min': 0,
-    'max': 4000,
-    'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
+        'min': 0,
+        'max': 4000,
+        'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
 
     # Create a folium map object.
     Map = folium.Map(location=[20, 0], zoom_start=3, height=500)
