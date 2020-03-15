9829dffc898b71fbfca58ca7bb4efeaccd759dc3
Qiusheng Wu <giswqs@gmail.com>
2020-03-15 17:08:31 -0400

    Updated basemaps

--- geehydro/geehydro.py
+++ geehydro/geehydro.py
@@ -7,7 +7,7 @@ import ee
 import folium
 from folium import plugins
 
-
+# Add Earth Engine layer to folium
 def addLayer(self, ee_object, vis_params={}, name='Layer untitled', shown=True, opacity=1):
 
     image = None
@@ -51,11 +51,46 @@ def addLayer(self, ee_object, vis_params={}, name='Layer untitled', shown=True, 
         opacity=opacity
     ).add_to(self)
 
-
-# Add EE drawing method to folium.
 folium.Map.addLayer = addLayer
 
+# Add custom WMS tile layer to folium
+def addWmsTileLayer(self, url, layers, name=None, attr='', overlay=True, control=True, show=True):
+    try:
+        folium.raster_layers.WmsTileLayer(
+            url=url,
+            layers=layers,
+            attr=attr,
+            name=name,
+            overlay=overlay,
+            control=control,
+            show=show
+        ).add_to(self)
+    except:
+        print("Failed to add the specified WMS TileLayer.")
 
+folium.Map.addWmsTileLayer = addWmsTileLayer
+
+
+# Add custom tile layer to folium
+def addTileLayer(self, tiles='OpenStreetMap', name=None, attr='', overlay=True, control=True, show=True, opacity=1, API_key=None):
+    try:
+        folium.raster_layers.TileLayer(
+            tiles=tiles,
+            name=name,
+            attr=attr,
+            overlay=overlay,
+            control=control,
+            show=show,
+            opacity=opacity,
+            API_key=API_key
+        ).add_to(self)
+    except:
+        print("Failed to add the specified TileLayer.")
+
+folium.Map.addTileLayer = addTileLayer
+
+
+# https://viewer.nationalmap.gov/services/
 # Modifies the Google Maps basemap
 # A mapTypeId to set the basemap to. Can be one of "ROADMAP", "SATELLITE", "HYBRID" or "TERRAIN" to select one of the standard Google Maps API map types
 def setOptions(self, mapTypeId='HYBRID', styles={}, types=[]):
@@ -95,18 +130,112 @@ def setOptions(self, mapTypeId='HYBRID', styles={}, types=[]):
             name='Esri Satellite',
             overlay=True,
             control=True
-        )
+        ),
+        'Esri Ocean': folium.TileLayer(
+            tiles='https://services.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Ocean',
+            overlay=True,
+            control=True
+        ),
+        'Esri Satellite': folium.TileLayer(
+            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Satellite',
+            overlay=True,
+            control=True
+        ),
+         'Esri Standard': folium.TileLayer(
+            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Standard',
+            overlay=True,
+            control=True
+        ),
+        'Esri Terrain': folium.TileLayer(
+            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/World_Terrain_Base/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Terrain',
+            overlay=True,
+            control=True
+        ),
+        'Esri Transportation': folium.TileLayer(
+            tiles='https://server.arcgisonline.com/ArcGIS/rest/services/Reference/World_Transportation/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Transportation',
+            overlay=True,
+            control=True
+        ),
+
+        'Esri Topo World': folium.TileLayer(
+            tiles='https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Topo World',
+            overlay=True,
+            control=True
+        ),
+
+        'Esri National Geographic': folium.TileLayer(
+            tiles='http://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri National Geographic',
+            overlay=True,
+            control=True
+        ),     
+
+        'Esri Shaded Relief': folium.TileLayer(
+            tiles='https://services.arcgisonline.com/arcgis/rest/services/World_Shaded_Relief/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Shaded Relief',
+            overlay=True,
+            control=True
+        ),   
+
+         'Esri Physical Map': folium.TileLayer(
+            tiles='https://services.arcgisonline.com/arcgis/rest/services/World_Physical_Map/MapServer/tile/{z}/{y}/{x}',
+            attr='Esri',
+            name='Esri Physical Map',
+            overlay=True,
+            control=True
+        ),   
+
+        'Bing VirtualEarth': folium.TileLayer(
+            tiles='http://ecn.t3.tiles.virtualearth.net/tiles/a{q}.jpeg?g=1',
+            attr='Microsoft',
+            name='Bing VirtualEarth',
+            overlay=True,
+            control=True
+        ),
+
+        '3DEP Elevation': folium.WmsTileLayer(
+            url='https://elevation.nationalmap.gov/arcgis/services/3DEPElevation/ImageServer/WMSServer?',
+            layers = '3DEPElevation:None',
+            attr='USGS',
+            name='3DEP Elevation',
+            overlay=True,
+            control=True
+        ),    
+
+        'NAIP Imagery': folium.WmsTileLayer(
+            url='https://services.nationalmap.gov/arcgis/services/USGSNAIPImagery/ImageServer/WMSServer?',
+            layers = '0',
+            attr='USGS',
+            name='NAIP Imagery',
+            overlay=True,
+            control=True
+        ),           
     }
 
     try:
         basemaps[mapTypeId].add_to(self)
     except:
-        print('Basemap can only be one of "ROADMAP", "SATELLITE", "HYBRID", "TERRAIN", or "ESRI"')
+        print('Basemap can only be one of "ROADMAP", "SATELLITE", "HYBRID", "TERRAIN", "NAIP", or "ESRI"')
 
 
 folium.Map.setOptions = setOptions
 
 # show the map
+
 
 
 def setControlVisibility(self, layerControl=True, fullscreenControl=True, latLngPopup=True):
@@ -129,7 +258,7 @@ def setCenter(self, lon, lat, zoom=10):
 folium.Map.setCenter = setCenter
 
 
-def centerObject(self, ee_object, zoom):
+def centerObject(self, ee_object, zoom=10):
     # try:
 
     lat = 0
