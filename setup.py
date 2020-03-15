c2460df9682ee083b55c259a7c1e2b3944571790
Qiusheng Wu <giswqs@gmail.com>
2020-03-15 17:11:20 -0400

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -64,6 +64,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/giswqs/geehydro',
-    version='0.1.4',
+    version='0.1.5',
     zip_safe=False,
 )
